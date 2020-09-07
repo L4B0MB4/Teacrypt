bab6ea3ef3454d6f53ad61b8b4490ee4d9fc84a1
L4B0MB4 <metalfan65@gmail.com>
2020-09-07 17:33:45 +0200

    implementing keyexchange encrypt and decrypt

--- backend/api/keyexchange/controller.ts
+++ backend/api/keyexchange/controller.ts
@@ -1,11 +1,34 @@
 import { Request, Response } from 'express';
 
+import * as ValidationUtils from '../../utils/validation';
 import * as service from './service';
 
-export const index = (_: Request, res: Response) => {
+const { body } = require("express-validator");
+
+export const getPublicKey = (_: Request, res: Response) => {
   try {
     const publicKey = service.getPublicKey();
     return res.json({ publicKey });
+  } catch (ex) {
+    return res.status(400).json({ status: 400, message: ex.message });
+  }
+};
+
+export const encryptValidation = ValidationUtils.validate([body("publicKey").notEmpty().isString()]);
+export const encrypt = (req: Request, res: Response) => {
+  try {
+    const encrypted = service.encrypt(Date.now().toString(), req.body.publicKey);
+    return res.json({ encrypted });
+  } catch (ex) {
+    return res.status(400).json({ status: 400, message: ex.message });
+  }
+};
+
+export const decryptValidation = ValidationUtils.validate([body("toDecrypt").notEmpty().isString()]);
+export const decrypt = (req: Request, res: Response) => {
+  try {
+    const decrypted = service.decrypt(req.body.toDecrypt);
+    return res.json({ decrypted });
   } catch (ex) {
     return res.status(400).json({ status: 400, message: ex.message });
   }
--- backend/main.ts
+++ backend/main.ts
@@ -1,46 +1,22 @@
 import * as express from 'express';
-import * as NodeRSA from 'node-rsa';
 
 import { ApiRouter } from './api/route';
-import { RSA_KEYS } from './keys';
 
 // the types are wrong for the express-validator
-const { query, validationResult } = require("express-validator");
+const { query } = require("express-validator");
+
 const port = 3000;
-const key = new NodeRSA({ b: 2064 });
-
-key.importKey(RSA_KEYS.PRIVATE, "private");
-
-key.importKey(RSA_KEYS.PUBLIC, "public");
-
-const text = "Hello RSA!";
-const encrypted = key.encrypt(text, "base64");
-console.log("encrypted: ", encrypted);
-const decrypted = key.decrypt(encrypted, "utf8");
-console.log("decrypted: ", decrypted);
 
 const app = express();
 
 app.use(express.json());
 
-const validate = (validations: Array<any>) => {
-  return async (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    await Promise.all(validations.map((validation) => validation.run(req)));
-
-    const errors = validationResult(req);
-    if (errors.isEmpty()) {
-      return next();
-    }
-
-    res.status(400).json({ errors: errors.array() });
-  };
-};
-
 app.use("/api", ApiRouter);
 
-app.get("/", validate([query("username").notEmpty().isString()]), (req: express.Request, res: express.Response) => {
-  return res.json({ username: req.query.username });
+app.get("/", (req: express.Request, res: express.Response) => {
+  return res.send("Welcome to Teacrypt");
 });
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
