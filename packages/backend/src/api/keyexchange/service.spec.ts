a55e1eece8d05ab0dec2007817a6862d71226872
L4B0MB4 <metalfan65@gmail.com>
2020-09-29 21:02:13 +0200

    adding participantkey exchange test

--- packages/backend/src/api/keyexchange/service.spec.ts
+++ packages/backend/src/api/keyexchange/service.spec.ts
@@ -1,5 +1,5 @@
 import mockingoose from 'mockingoose';
-import { mongo } from 'mongoose';
+import { mongo, Schema } from 'mongoose';
 
 import { IUser, UserModel } from '../authentication/model';
 import { KeyModel } from './model';
@@ -19,5 +19,15 @@ describe("KeyExchange Service", () => {
     mockingoose(UserModel).toReturn({ _id: "p_Id" }, "findOne");
     const result = await Service.shareAESKey("pKey", "pId", { _id: new mongo.ObjectID() } as IUser);
     expect(result).toBe(true);
+  });
+
+  it("getting participant keys", async () => {
+    KeyModel.schema.path("sharer", Object);
+    mockingoose(KeyModel).toReturn([{ _id: "p_Id", sharer: { id: "xxx" }, participantKey: "myKey" }], "find");
+    let result = await Service.getParticipantKeys(("my_Id" as unknown) as Schema.Types.ObjectId);
+    expect(result).toHaveLength(1);
+    mockingoose(KeyModel).toReturn([], "find");
+    result = await Service.getParticipantKeys(("my_Id" as unknown) as Schema.Types.ObjectId);
+    expect(result).toHaveLength(0);
   });
 });
